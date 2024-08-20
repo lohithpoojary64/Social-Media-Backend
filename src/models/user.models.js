08c5997f71c008833e79e3c3b6ffaf86e460eda0
lohithpoojary64 <lohithpoojary64@gmail.com>
2024-08-20 12:24:24 +0530

    Created user and video models and added password hashing functionality

---
+++ src/models/user.models.js
@@ -0,0 +1,76 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import { Jwt } from "jsonwebtoken";
+//username , email , fullname , password , watchHistory ,
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true,
+      trim: true, //we use trim to avoid unnessesary spacing in the username
+      index: true, //basically it is used to retrive data fast
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    Profile: {
+      type: String,
+    },
+    video: {
+      type: Schema.Types.ObjectId,
+      ref: "Video",
+    },
+    watchHistory: [(type = String)],
+    likes: {
+      type: Number,
+      default: 0,
+    },
+    dislikes: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+//Here I will hash my password and I will create a web token.
+//I cannot create do it directly , so to do so I need to create one middleware
+//I want the middle ware to handle so tasks before the user submits the password.
+userSchema.pre("save", async function (next) {
+  if(!this.isModified("password")){
+    return next();
+  }
+  this.password = bcrypt.hash(this.password);
+  next();
+});
+
+//now I need to compare the password of the user from the hashed password ,that I got from bcrypt.
+//So to do so I need to use methods . Since the schema provides access to many methods .
+userSchema.methods.comparePassword = async function(password){
+  try {
+    await bcrypt.compare(password,this.password) //bcrypt provides a method to compare the original password and the hashed password . that is .compare method.
+    
+  } catch (error) {
+    console.log('There is an error while comparing the original and hashed password',error);
+    throw error;
+    
+  }
+}
+
+export const User = mongoose.model("User", userSchema);
